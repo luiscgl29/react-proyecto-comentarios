a4974d2c0b98dd90e2f2757ebad7a4159bd2617e
Luis Garcia <luiscarlopz@gmail.com>
2022-08-30 13:11:46 -0600

    Finalización proyecto testimonio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import "./App.css";
-import Testimonio from "./componentes/Testimonio.js";
+import './App.css';
+import Testimonio from './componentes/Testimonio.js';
 
 function App() {
   return (
-    <div className="App">
-      <div className="contenedor-principal">
+    <div className='App'>
+      <div className='contenedor-principal'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio />
+
+        <Testimonio
+          nombre='Shawn Wang'
+          pais='Singapur'
+          imagen='shawn'
+          cargo='Ingeniero de Software'
+          empresa='Amazon'
+          testimonio='"Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. <b>freeCodeCamp cambió mi vida.</b>'
+        />
+
+        <Testimonio
+          nombre='Sarah Chima'
+          pais='Nigeria'
+          imagen='sarah'
+          cargo='Ingeniera de Software'
+          empresa='ChatDesk'
+          testimonio='<b>freeCodeCamp fue la puerta de entrada a mi carrera</b> como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
+        />
+
+        <Testimonio
+          nombre='Emma Bostian'
+          pais='Suecia'
+          imagen='emma'
+          cargo='Ingeniera de Software'
+          empresa='Spotify'
+          testimonio='"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en
+          <b>freeCodeCamp me dio las habilidades</b> y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify"'
+        />
       </div>
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,26 +1,26 @@
-import React from "react";
-import "../hojas-de-estilo/Testimonio.css";
+import React from 'react';
+import '../hojas-de-estilo/Testimonio.css';
 
-function Testimonio() {
+function Testimonio(props) {
   return (
-    <div className="contenedor-testimonio">
+    <div className='contenedor-testimonio'>
       <img
-        className="imagen-testimonio"
-        src={require("../imagenes/testimonio-emma.png")}
-        alt="Foto de Emma"
+        className='imagen-testimonio'
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt={`Foto de ${props.imagen}`}
       />
 
-      <div className="contenedor-texto-testimonio">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
-        <p className="texto-testimonio">
-          "Siempre he tenido problemas para aprender JavaScript. He tomado
-          muchos cursos, pero el curso de freeCodeCamp fue el que se quedó.
-          Estudiar JavaScript, así como estructuras de datos y algoritmos en
-          freeCodeCamp me dio las habilidades y la confianza que necesitaba para
-          conseguir el trabajo de mis sueños como ingeniero de software en
-          Spotify"
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}
         </p>
+        <p className='cargo-testimonio'>
+          {props.cargo} en <strong>{props.empresa}</strong>
+        </p>
+        <p
+          className='texto-testimonio'
+          dangerouslySetInnerHTML={{ __html: props.testimonio }} //dangerouslySetInnerHTML hace que el texto normal se convierta en html, lo inyecta para que las etiquetas validen su funcionamiento, en este caso negrita. Por otro lado dentro de las etiquetas de "p" y sin dangerously debe ir "{props.textimonio}" para validar el testimonio sin negrita, las comillas únicamente son para marcar que se trata de una frase.
+        ></p>
       </div>
     </div>
   );
